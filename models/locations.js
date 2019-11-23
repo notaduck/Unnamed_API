ff073fb962c099e34f3f583382741e1d96d99848
Daniel Guldberg Aaes <guldberg72@gmail.com>
2019-11-23 23:39:15 +0100

    basic location model and validator has been added

---
+++ models/locations.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+
+const Location = mongoose.model('Location', new mongoose.Schema({
+	id: {
+		type: Number,
+	},
+	coordinate: {
+		latitude: {
+			type: Number,
+		},
+		longitude: {
+			type: Number,
+		},
+	},
+	title: {
+		type: String,
+	},
+	description: {
+		type: String,
+	},
+	IsAPlayground: Boolean,
+
+}));
+
+function validateLocation(location) {
+	const schema = {
+		coordinate: Joi.object().keys({
+			longitude: Joi.number().min(-180).max(180).required(),
+			latitude: Joi.number().min(-90).max(90).required(),
+		}),
+		title: Joi.string().min(5).max(1000).required(),
+		description: Joi.string().min(5).max(500).required(),
+		isAPlayground: Joi.bool().required(),
+	};
+
+	return Joi.validate(location, schema);
+}
+
+exports.Marker = Location;
+exports.validate = validateLocation;
