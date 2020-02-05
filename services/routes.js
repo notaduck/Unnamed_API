12e0b91cd0e0fdc7f4d3686d7ade2235e95e31b7
Daniel Guldberg Aaes <guldberg72@gmail.com>
2020-02-05 23:15:27 +0100

    the user route has been added

--- services/routes.js
+++ services/routes.js
@@ -2,12 +2,16 @@ const cors = require('cors');
 const express = require('express');
 
 const locations = require('../routes/locations.js');
+const users = require('../routes/userss');
 
 // const error = require('../middleware/error.js');
 
 module.exports = function (app) {
+
+	app.use(express.json());
+
 	// I need to read up on cors before !!!!
 	// https://flaviocopes.com/express-cors/
-	app.use(express.json());
 	app.use('/api/locations', cors(), locations);
+	app.use('/api/users', cors(), users);
 };
