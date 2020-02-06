fef70e4c5e80b8bc2588c495acc6febd6a6aabf4
Daniel Guldberg Aaes <guldberg72@gmail.com>
2020-02-06 12:42:02 +0100

    formatted

--- routes/locations.js
+++ routes/locations.js
@@ -7,26 +7,34 @@ const titleFormatter = require('../utils/titleFormatter');
 const router = express();
 
 router.get('/', async (req, res) => {
+
 	const locations = await Location.find();
+
 	res.status(HttpStatus.OK).json(locations);
+
 });
 
 router.get('/:id', async (req, res) => {
+
 	const location = await Location.findById(req.params.id);
+
 	if (!location)
-		res
-			.status(HttpStatus.NOT_FOUND)
+		res.status(HttpStatus.NOT_FOUND)
 			.send('No location with the given id was found');
+
 	res.status(HttpStatus.OK).json(location);
+
 });
 
 router.post('/', async (req, res) => {
+
 	const { error } = validate(req.body);
 
 	if (error)
 		return res.status(HttpStatus.BAD_REQUEST).send(error.details[0].message);
 
 	const formattedTitle = titleFormatter(req.body.title);
+
 	let location = new Location({
 		coordinate: {
 			longitude: req.body.coordinate.longitude,
@@ -41,6 +49,7 @@ router.post('/', async (req, res) => {
 	location = await location.save();
 
 	res.status(HttpStatus.OK).json(location);
+
 });
 
 module.exports = router;
