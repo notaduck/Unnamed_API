c7744ffac2732aee3f0563c5b0a3dad4307e2be9
Daniel Guldberg Aaes <guldberg72@gmail.com>
2020-02-06 12:40:56 +0100

    a jwt token is now returned

--- routes/userss.js
+++ routes/userss.js
@@ -2,17 +2,18 @@ const express = require('express');
 const router = express();
 const { User, validate } = require('../models/user');
 const HttpStatus = require('http-status-codes');
+const jwt = require('jsonwebtoken');
 
 require('express-async-errors');
 
-router.post('/signup', async(req, res) => {
+router.post('/signup', async (req, res) => {
 
 	const { error } = validate(req.body);
 	if (error) return res.status(HttpStatus.BAD_REQUEST).send(error.details[0].message);
 
-	const {name, email, password, passwordConfirm} = req.body;
+	const { name, email, password, passwordConfirm } = req.body;
 
-	let user = await User.findOne({email});
+	let user = await User.findOne({ email });
 	if (user) return res.status(HttpStatus.BAD_REQUEST).send('User already registered');
 
 	user = new User({
@@ -24,7 +25,21 @@ router.post('/signup', async(req, res) => {
 
 	await user.save();
 
-	res.status(HttpStatus.CREATED).send(user);
+	const token = jwt.sign({ id: user._id }, 'JWT SECRET', {
+		expiresIn: process.env.JWT_EXPIRES_IN
+	});
+
+	res.status(HttpStatus.CREATED).json({
+		token,
+		data: {
+			user: user
+		}
+	});
+	// res.status(HttpStatus.CREATED).send(user);
+});
+
+router.get('/', (req, res) => {
+	res.send(process.env.JWT_EXPIRES_IN);
 });
 
 module.exports = router;
