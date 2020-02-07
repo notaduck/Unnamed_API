58aece7c63a6f90d3683e96b6b28d96760d03ad1
Daniel Guldberg Aaes <guldberg72@gmail.com>
2020-02-07 12:58:09 +0100

    a user cano request a new password, passwordRest still needs to implemented

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express();
-const { User, validate } = require('../models/user');
 const HttpStatus = require('http-status-codes');
 const Joi = require('joi');
-const { auth } = require('../middelware/auth');
 
+const { User, validate } = require('../models/user');
+const { auth } = require('../middelware/auth');
+const sendMail = require('../utils/email');
 router.post('/signup', async (req, res) => {
 
 	const { error } = validate(req.body);
@@ -52,12 +53,55 @@ router.get('/login', async (req, res) => {
 
 });
 
-router.post('/msg', auth, async (req, res) => {
-	res.status(HttpStatus.OK).json({
-		msg: 'msg called',
+router.post('/forgotPassword', async (req, res) => {
+
+	const { email, password } = req.body;
+	const { error } = validateForgotPassword(req.body);
+
+	if (error) return res.status(HttpStatus.BAD_REQUEST).send(error.details[0].message);
+
+	let user = await User.findOne({ email });
+	if (!user) return res.status(HttpStatus.BAD_REQUEST).send('Email is not found.');
+
+
+	// Generate reset token
+	const reset_token = user.createPasswordResetToken();
+	await user.save({ validateBeforeSave: false }); // disable schema validation.
+
+	const reset_url = `${req.protocol}://${req.get('host')}/api/users/resetPassword/${reset_token}`;
+	const message = `Forgot your password?\nSubmit a new one from: ${reset_url}\nif you didn't, please ifnore this email`;
+
+	await sendMail({
+		email,
+		subject: 'Your password reset token, valid for 10 minutes.',
+		message
 	});
 
+	res.status(HttpStatus.OK).json({
+		status: 'success',
+		message: `token send to ${user.email}`
+	});
+
+
 });
+
+router.patch('/resetPassword/:token', async (req, res) => {
+
+});
+
+// router.post('/msg', auth, async (req, res) => {
+// 	res.status(HttpStatus.OK).json({
+// 		msg: 'msg called',
+// 	});
+
+// });
+const validateForgotPassword = (user) => {
+	const schema = {
+		email: Joi.string().min(5).max(255).required().email()
+		// password: Joi.string().min(5).max(255).required(),
+	};
+	return Joi.validate(user, schema);
+};
 
 const validateEmailandPassword = (user) => {
 	const schema = {
